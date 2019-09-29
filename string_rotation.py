8920f17ca34ed46f8283f426f877aa9d4a1ff43a
Tomas Ribinskis <tomas.ribinskis@gmail.com>
2019-09-29 17:15:01 +0300

    Rotation chalenge completed

--- string_rotation.py
+++ string_rotation.py
@@ -1,4 +1,27 @@
+#Correct solution:
+#def rotate(string, n):
+#    return string[n:] + string[:n]
+
+
 import collections
+
+def clean_string(rotated_string):
+    other_string = ''
+    symbol_check = False
+    for i in rotated_string:
+        if i=='!':
+            other_string += i
+            symbol_check = False
+        elif i==' ':
+            if symbol_check:
+                other_string += i
+                symbol_check = False
+                continue
+            symbol_check = True
+        elif i.isalpha():
+            symbol_check = False
+            other_string += i
+    return other_string
 
 def rotate(string, n):
     """Rotate characters in a string.
@@ -7,65 +30,42 @@ def rotate(string, n):
     if n>0:
         sliced_string1 = list(string[n:])
         sliced_string2 = list(string.replace(string[n:], ''))
-        print(sliced_string1, sliced_string2)
-        clean_string = str(sliced_string1 + sliced_string2)
-
-        temp_string = ''
-        other_string = ''
-        print(clean_string, 0)
-        for i in clean_string:
-            print(i, 1)
-            if i.isalpha():
-                other_string += i
-                print(other_string, 2)
-            #else:
-            #    other_string += temp_string
-            #    print(other_string, 3)
-
-
-        print(other_string, 4)
-        #clean_string = clean_string.replace('[', '')
-        #clean_string = clean_string.replace("'", '')
-        #clean_string = clean_string.replace(']', '')
-        #clean_string = clean_string.replace(',', '')
-        #clean_string = clean_string.replace(' ', '')
-        result_string = other_string
+        rotated_string = str(sliced_string1 + sliced_string2)
+        result_string = clean_string(rotated_string)
     elif n<0:
-        sliced_string = string[:abs(n)]
-        clean_string = string.replace(sliced_string, '')
-        result_string = clean_string + sliced_string
-    else:
-        result_string = string
+        sliced_string1 = list(string[:n])
+        sliced_string2 = list(string.replace(string[:n], ''))
+        rotated_string = str(sliced_string2 + sliced_string1)
+        result_string = clean_string(rotated_string)
 
     return result_string
 
-
-#print(rotate('bob and julian love pybites!', 15))
-print(rotate('bob and julian love pybites!', 15), 5)
 
 def test_small_rotate():
     assert rotate('hello', 2) == 'llohe'
     assert rotate('hello', -2) == 'lohel'
 
-#print(test_small_rotate())
+print(test_small_rotate())
 
 def test_bigger_rotation_of_positive_n():
     string = 'bob and julian love pybites!'
     expected = 'love pybites!bob and julian '
     assert rotate(string, 15) == expected
 
-#print(test_bigger_rotation_of_positive_n())
+print(test_bigger_rotation_of_positive_n())
 
 def test_bigger_rotation_of_negative_n():
     string = 'pybites loves julian and bob!'
     expected = 'julian and bob!pybites loves '
     assert rotate(string, -15) == expected
 
+print(test_bigger_rotation_of_negative_n())
 
 def test_rotation_of_n_same_as_len_str():
     string = expected = 'julian and bob!'
     assert rotate(string, len(string)) == expected
 
+print(test_rotation_of_n_same_as_len_str())
 
 def test_rotation_of_n_bigger_than_string():
     string = 'julian and bob!'
@@ -79,3 +79,5 @@ def test_rotation_of_n_bigger_than_string():
     mod = 100 % len(string)  # 10
     assert rotate(string, mod) in (expected_solution1,
                                    expected_solution2)
+
+print(test_rotation_of_n_bigger_than_string())
