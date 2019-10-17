62b82465736f687f964f3d6165d5da1e2cf39e2d
Tomas Ribinskis <tomas.ribinskis@gmail.com>
2019-10-17 18:55:11 +0300

    Alphanumeric challenge update

--- alpha_numeric.py
+++ alpha_numeric.py
@@ -1,3 +1,18 @@
+#Corret solution
+import string
+
+alphanumeric_chars = list(string.ascii_letters + string.digits)
+
+def get_index_different_char(chars):
+    matches, no_matches = [], []
+    for i, char in enumerate(chars):
+        if str(char).lower() in alphanumeric_chars:
+            matches.append(i)
+        else:
+            no_matches.append(i)
+    return matches[0] if len(matches) == 1 else no_matches[0]
+
+#My solution
 def get_index_different_char(chars):
     alpha_chars = []
     not_alpha_chars = []
